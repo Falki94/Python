f3b8b9373e65cc6fc44418cb149a4cdc8273db10
Maciej Falkiewicz <falkiewicz@TRIA.local>
2020-10-01 13:00:04 +0200

    yeah

---
+++ DuzyLotek.py
@@ -0,0 +1,46 @@
+import random
+
+try:
+    ileliczb = int(input("Podaj ilość typowanych liczb: "))
+    maksliczba = int(input("Podaj maksymalną losowana liczbe: "))
+    # print('Wytypuj', ileliczb, 'z', maksliczba, ' liczb: ')
+    if ileliczb > maksliczba:
+        print("Błedne dane")
+        exit()
+except ValueError:
+    print("Błędne dane")
+    exit()
+
+liczby = []
+i = 0
+while i < ileliczb:
+    liczba = random.randint(1, maksliczba)
+    if liczby.count(liczba) == 0:
+        liczby.append(liczba)
+        i = i + 1
+
+for i in range(3):
+    print("Wytypuj %s z %s liczb: " % (ileliczb, maksliczba))
+    typy = set()
+    i = 0
+    while i < ileliczb:
+        try:
+            typ = int(input("Podaj liczbę %s: " % (i + 1)))
+        except ValueError:
+            print("Błędne dane!")
+            continue
+
+        if 0 < typ <= maksliczba and typ not in typy:
+            typy.add(typ)
+            i = i + 1
+
+    trafione = set(liczby) & typy
+    if trafione:
+        print("\nIlość trafień: %s" % len(trafione))
+        print("Trafione liczby: ", trafione)
+    else:
+        print("Brak trafień. Spróbuj jeszze raz!")
+
+    print("\n" + 'x' * 40 + "\n")  # Wydrukuj 40 znaków
+
+print("Wylosowane liczby: ", liczby)
